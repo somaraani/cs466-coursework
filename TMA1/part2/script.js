1e2a63b9cdfb24443a3475c305d2556794e9062b
Somar Aani <somar.aani@ibm.com>
2020-03-19 16:07:57 -0400

    Added quiz

--- TMA1/part2/script.js
+++ TMA1/part2/script.js
@@ -1,33 +1,72 @@
+var currLink = undefined;
+
 function display(link, obj, i) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', link, true);
-    httpRequest.onload = function () {
-        var content = httpRequest.responseText;
-        document.querySelector('#content').innerHTML = content;
-    };
-    httpRequest.send();
 
-    // make current selected
-    if (obj != null) {
-        document.getElementsByClassName('selected')[0].classList.remove("selected");
+    if (currLink != link) {
+        // get content from local file
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET', link + ".html", true);
+        httpRequest.onload = function () {
+            var content = httpRequest.responseText;
+            document.querySelector('#content').innerHTML = content;
+        };
+        httpRequest.send();
 
-        var node = obj.parentElement;
-        while (true) {
-            if(node.classList && node.classList.contains('header')) {
-                node.classList.add('selected');
-                break;
+        // make current section selected
+        if (obj != null) {
+            document.getElementsByClassName('selected')[0].classList.remove("selected");
+            var node = obj.parentElement;
+            while (true) {
+                if (node.classList && node.classList.contains('header')) {
+                    node.classList.add('selected');
+                    break;
+                }
+                node = node.previousSibling;
             }
-
-            node = node.previousSibling;
         }
 
+        currLink = link;
+        quiz(link);
     }
 
-    if (i != null) {
-        document.getElementById(i).scrollIntoView({
-            behavior: 'smooth'
-        });
-    }
+    // scroll to correct section
+    window.setTimeout(function () {
+        if (i != null) {
+            document.getElementById(i).scrollIntoView({
+                behavior: 'smooth'
+            });
+        } else {
+            window.scrollTo({ top: '0', behavior: 'smooth' });
+        }
+    }, 100);
 }
 
-window.addEventListener("load", display("home.html"), false);
+function quiz(name) {
+    // get content from local file
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', name + '.xml', true);
+    httpRequest.onload = function () {
+        var quiz = document.getElementById("quiz");
+        quiz.innerHTML += "<form>";
+
+        var content = httpRequest.responseXML;
+        var questions = content.getElementsByTagName("question");
+
+        for (let index = 0; index < questions.length; index++) {
+            const question = questions[index].getElementsByTagName("description")[0].firstChild.nodeValue;
+            quiz.innerHTML += "<h1>" + question + "</h1>"
+            const answers = questions[index].getElementsByTagName("option");
+            for (let ans = 0; ans < answers.length; ans++) {
+                const answerText = answers[ans].firstChild.nodeValue;
+                quiz.innerHTML += '<label><input name="' + index + '" type="radio"/>' + answerText + '<label/>';
+            }
+        }
+
+        quiz.innerHTML += '<input class="submit" type="submit"/>'
+        quiz.innerHTML += "<form/>"
+
+    };
+    httpRequest.send();
+}
+
+window.addEventListener("load", display("home"), false);
