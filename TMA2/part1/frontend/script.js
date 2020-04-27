6c8aba4856cf92876738c4c8dad2c0f7c1ce9ebb
Somar Aani <somarani.1998@gmail.com>
2020-04-26 22:01:44 -0400

    Finished TMA2/Part1

--- TMA2/part1/frontend/script.js
+++ TMA2/part1/frontend/script.js
@@ -1,7 +1,13 @@
+var editing = -1;
 
 function hideOverlay() {
+    document.getElementById("name").value = "";
+    document.getElementById("url").value = "";  
     document.getElementById("dimmer").hidden = true;
-    document.getElementById("overlay").hidden = true;   
+    document.getElementById("overlay").hidden = true;
+    editing = -1;
+    document.getElementById("edit-id").value = editing;
+    resize();
 }
 
 function showOverlay() {
@@ -9,8 +15,29 @@ function showOverlay() {
     document.getElementById("overlay").hidden = false;   
 }
 
-function create() {
+function add() {
     showOverlay();
+    editing = -1;
+    document.getElementById("edit-id").value = editing;
 }
 
+function edit(id, name, url) {
+    showOverlay();
+    editing = id;
+    document.getElementById("name").value = name;
+    document.getElementById("url").value = "http://www." + url;
+    document.getElementById("edit-id").value = editing;
+}
+
+function resize(){
+    var containers = document.getElementsByClassName("bookmark");
+    for(var i=0;i<containers.length;i++) {
+        console.log(containers[i].offsetWidth);
+        if(containers[i].offsetWidth > 390) {
+            containers[i].style.width = "790px";
+        }
+    }
+}
+
+window.onresize = resize;
 window.addEventListener("load", hideOverlay, false);
